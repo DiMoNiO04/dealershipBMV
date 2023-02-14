56fac5819b9ea0523e603968904143a53e85cd59
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-02-14 22:08:07 +0300

    feat: add service page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Скрол хедера изменение фона
 const header = document.querySelector('.header')
 const headerContainer = document.querySelector('.header__container')
 const headerLogout = document.querySelector('.header__logout') 
@@ -12,4 +13,23 @@ window.onscroll = () => {
 		headerContainer.classList.remove('header__container__scrool');
 		headerLogout.classList.remove('header__logout__scroll')
 	}
+}
+
+
+//На странице услуги и сервис изменение значков плюс и минус
+const coll = document.querySelectorAll('.service__collapsible')
+const icon = document.querySelectorAll('.fa-plus')
+for(let i = 0; i < coll.length; i++){
+	coll[i].addEventListener('click', function(){
+		let content = this.nextElementSibling;
+		if(content.style.maxHeight){
+			content.style.maxHeight = null;
+			icon[i].classList.remove('fa-minus');
+			icon[i].classList.add('fa-plus')
+		}else{
+			content.style.maxHeight = content.scrollHeight + 'px';
+			icon[i].classList.remove('fa-plus');
+			icon[i].classList.add('fa-minus')
+		}
+	})
 }
