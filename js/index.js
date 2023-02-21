3c3abff43a5e53053dc9cff63d839659a8db4bf6
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-02-21 18:22:04 +0300

    feat: add auto page

--- js/index.js
+++ js/index.js
@@ -33,3 +33,25 @@ for(let i = 0; i < coll.length; i++){
 		}
 	})
 }
+
+
+//Активная ссылка в сайдбаре
+const sidebar = document.querySelector('.sidebar__list')
+const sidebarItems = document.querySelectorAll('.sidebar__item')
+
+
+const q = (index) => {
+	for(let i = 0; i<sidebarItems.length; i++){
+		sidebarItems[i].classList.remove('sidebar__item-active')
+	}
+	sidebarItems[index].classList.add('sidebar__item-active')
+}
+
+
+sidebar.addEventListener('click',  e => {
+	let id = e.target.id
+	for(let i = 0; i<sidebarItems.length; i++){
+		sidebarItems[i].classList.remove('sidebar__item-active')
+	}
+	sidebarItems[id].classList.add('sidebar__item-active')
+})
